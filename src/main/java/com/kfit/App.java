3d69f62e9567641b6ee0d678a7cc1d2c40b4d56a
allenleung86 <londalonda@qq.com>
2017-04-22 16:33:24 +0800

    使Spring Boot完美使用FastJson解析JSON数据

--- src/main/java/com/kfit/App.java
+++ src/main/java/com/kfit/App.java
@@ -1,7 +1,13 @@
 package com.kfit;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 //其中@SpringBootApplication申明让spring boot自动给程序进行必要的配置，等价于以默认属性使用@Configuration，@EnableAutoConfiguration和@ComponentScan
 @SpringBootApplication
 public class App {
+    /**
+     * @title: 使用FastJson解析JSON数据
+     * @Author: Allen
+     * @Date: 2017/4/22 0022 下午 3:36
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
